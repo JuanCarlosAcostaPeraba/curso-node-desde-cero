dbc868577b49153dd256c7dfa9ea95e276caef37
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2023-09-07 15:35:40 +0100

    Add CommonJS files

---
+++ cjs/calc.js
@@ -0,0 +1,17 @@
+function sum(a, b) {
+	return a + b;
+}
+
+function sub(a, b) {
+	return a - b;
+}
+
+function mul(a, b) {
+	return a * b;
+}
+
+module.exports = {
+	sum,
+	sub,
+	mul
+}
---
+++ cjs/index.js
@@ -0,0 +1,5 @@
+const { sum, sub, mul } = require('./calc')
+
+console.log(sum(1, 2))
+console.log(sub(1, 2))
+console.log(mul(1, 2))
